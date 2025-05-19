b1f3a453ead1c19c1f1ede2b0d214e5fa2df5fab
deepesh-sr <rathoredeepeshsingh@gmail.com>
2025-05-20 03:03:50 +0530

    done till 03_if :)

--- exercises/01_variables/variables6.rs
+++ exercises/01_variables/variables6.rs
@@ -1,6 +1,9 @@
 // TODO: Change the line below to fix the compiler error.
-const NUMBER = 3;
+const NUMBER :i32 = 3;
 
 fn main() {
     println!("Number: {NUMBER}");
 }
+
+// Explaination : In rust we always have to give type , whether its numbere , string 
+// We can give type by using " : " keywords next to the variable name.
