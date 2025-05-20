9ac8c43b9bb0f3da9e3615a81e16aec7211c568f
deepesh-sr <220714100078@cutm.ac.in>
2025-05-20 13:49:55 +0530

    05_vectors done -_-

--- exercises/04_primitive_types/primitive_types1.rs
+++ exercises/04_primitive_types/primitive_types1.rs
@@ -8,8 +8,10 @@ fn main() {
 
     // TODO: Define a boolean variable with the name `is_evening` before the `if` statement below.
     // The value of the variable should be the negation (opposite) of `is_morning`.
-    // let …
+    let is_evening : bool = true;
     if is_evening {
         println!("Good evening!");
     }
 }
+
+// Explaination : Simple decalare the variable nothing fancy.
